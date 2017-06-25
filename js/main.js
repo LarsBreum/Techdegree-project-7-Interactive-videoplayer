9d74c3497099b7b608fbea36bd8d9c2f4b4f3111
Lars Breum Hansen <lars.breum.lh@gmail.com>
2017-06-25 07:06:06 +0200

    grabbed all paragraps

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 $('video').mediaelementplayer();
-	
 
-$('video').on('play', function () {
-	console.log("video is playing");
-});
+$(document).ready( function () {
+	function getElements (selector) {
+		return document.querySelectorAll(selector);
+	}
+
+	const p = getElements('p');
+	console.log(p);
+
+		
+
+	$('video').on('play', function () {
+		console.log("video is playing");
+	});
+})
+	
